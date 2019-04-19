2bf376e90def2cb821644f07aafd8c467055db69
Jrej <jrejoire@hotmail.fr>
2019-04-19 03:33:27 +0200

    Updates

---
+++ precache-manifest.6188e85e96bf672adcd60ae0da09035d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47afcf7c1fdce8fc928a",
+    "url": "/Robofriends/static/js/runtime~main.4fea8ee0.js"
+  },
+  {
+    "revision": "dd649ebf8e520f8e25e6",
+    "url": "/Robofriends/static/js/main.597135b9.chunk.js"
+  },
+  {
+    "revision": "242bd478ec811df76fa9",
+    "url": "/Robofriends/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "dd649ebf8e520f8e25e6",
+    "url": "/Robofriends/static/css/main.16facb75.chunk.css"
+  },
+  {
+    "revision": "242bd478ec811df76fa9",
+    "url": "/Robofriends/static/css/2.4690a816.chunk.css"
+  },
+  {
+    "revision": "8ae9b6b227b9374f7290019b14c31f47",
+    "url": "/Robofriends/index.html"
+  }
+];
